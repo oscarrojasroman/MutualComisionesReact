8f73840d8240c6c5a138187dda517d4433db476f
oscarojasroman <oesrr69@gmail.com>
2018-10-09 17:09:38 -0300

    File Pond css

--- src/components/Datos/CargaDeDatos.jsx
+++ src/components/Datos/CargaDeDatos.jsx
@@ -5,48 +5,46 @@ import Grid from '@material-ui/core/Grid';
 import 'react-tabs/style/react-tabs.css';
 import './CargaDeDatos.css'
 import { IoIosDocument, IoIosCloudUpload } from 'react-icons/io';
-import { FilePond, File, registerPlugin } from 'react-filepond';
-import 'filepond/dist/filepond.min.css';
+import { FilePond } from 'react-filepond';
+import './filepound.css';
 
 const styles = theme => ({
   root: {
     flexGrow: 1,
   },
-  paper: {
-    padding: theme.spacing.unit * 2,
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  },
 });
+
+const icon = <IoIosCloudUpload className="iconCloud"/>
+
 
 function FullWidthGrid(props) {
   const { classes } = props;
 
   return (
-  <div className={classes.root}>   
+    <div className={classes.root}>
       <Grid className="wrapper">
         <Grid className="calculateTable">
-          
-                <div className="form-group">
-                    <div className="card-header color-secundary">
-                      <p className="p"><IoIosDocument/> CARGA DE ARCHIVOS</p>
-                    </div>
-                    <div>
-                      <br/>
-                        <FilePond className="cargaArchivos" labelIdle='Cargar Archivo Cotizacion.xlsx'/>
-                      <br/>
-                        <FilePond className="cargaArchivos" labelIdle='Cargar Archivo Planta.xlsx'/>
-                      <br/>
-                        <FilePond className="cargaArchivos" labelIdle='Cargar Archivo Ventas.xlsx'/>
-                      <br/>
-                        <FilePond className="cargaArchivos" labelIdle='Cargar Archivo Referidos.xlsx'/>
-                      <br/>
-                      
-                    </div>                  
-                    <button className="btn btn-add-file p">Validar Archivos</button>
-                </div>
-          
-        </Grid>       
+
+          <div className="form-group">
+            <div className="card-header color-secundary">
+              <p className="p"><IoIosDocument /> CARGA DE ARCHIVOS</p>
+            </div>
+            <div>
+              <br />
+                <FilePond className="cargaArchivos" labelIdle='↑ Cargar Archivo Cotizacion.xlsx'/>
+              <br />
+                <FilePond className="cargaArchivos" labelIdle='↑ Cargar Archivo Planta.xlsx' />
+              <br />
+                <FilePond className="cargaArchivos" labelIdle='↑ Cargar Archivo Ventas.xlsx' />
+              <br />
+                <FilePond className="cargaArchivos" labelIdle='↑ Cargar Archivo Referidos.xlsx' />
+              <br />
+
+            </div>
+            <button className="btn btn-add-file p">Validar Archivos</button>
+          </div>
+
+        </Grid>
       </Grid>
     </div>
   );
