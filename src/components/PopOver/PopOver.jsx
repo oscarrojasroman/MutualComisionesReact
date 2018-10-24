11cd20aeffc6b59cbcc3cdd9b026cd23f5934fd3
oscarojasroman <oesrr69@gmail.com>
2018-10-24 10:41:50 -0300

    po

--- src/components/PopOver/PopOver.jsx
+++ src/components/PopOver/PopOver.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { Popover, Button ,OverlayTrigger} from 'react-bootstrap';
+import { Popover,OverlayTrigger} from 'react-bootstrap';
 import { IoIosHelpCircleOutline } from 'react-icons/io';
 import './PopOver.css';
 
